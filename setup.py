6cad5d978af6c7c4f74b8c4757ea3b3c8c879c2c
Jonathan Hacker <raveberry@jhacker.de>
2020-03-26 22:23:51 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open('raveberry/README.md', 'r') as f:
+    long_description = f.read()
+
+with open('raveberry/requirements.txt') as f:
+    required_packages = f.read().splitlines()
+#psycopg2 is not needed for the basic version
+required_packages = [package for package in required_packages if not package.startswith('psycopg2')]
+
+setuptools.setup(
+    name='raveberry',
+    version='0.3.0',
+    author='Jonathan Hacker',
+    author_email='raveberry@jhacker.de',
+    description='A multi-user music server with a focus on participation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/raveberry/raveberry',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django :: 2.2',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=['raveberry'],
+    include_package_data=True,
+    python_requires='>=3.7',
+    install_requires=required_packages,
+    scripts=['raveberry/bin/raveberry'],
+)
