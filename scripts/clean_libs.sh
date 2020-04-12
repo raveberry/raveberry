e7bf48943d70d9632ef6c94e4653f280e46eac94
Jonathan Hacker <raveberry@jhacker.de>
2020-04-12 22:49:40 +0200

    clean static folder before packaging

---
+++ scripts/clean_libs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd static/libs || exit 1
+
+KEEP=$(cat <<-EOF
+	jquery/dist/jquery.min.js$
+	bootstrap/dist/js/bootstrap.min.js$
+	jquery-ui-dist/jquery-ui.min.js$
+	jquery-ui-touch-punch/jquery.ui.touch-punch.min.js$
+	jquerykeyframes/dist/jquery.keyframes.js$
+	js-cookie/src/js.cookie.js$
+	reconnecting-websocket/dist/reconnecting-websocket-iife.min.js$
+	bootstrap/scss/.*
+	@fortawesome/fontawesome-free/webfonts/.*.woff2
+	@fortawesome/fontawesome-free/scss/.*
+EOF
+)
+
+KEEP=$(echo "$KEEP" | sed -z 's/\n/\\|/g' | sed 's/\\|$//')
+
+find . -type f,l | grep -v "$KEEP" | xargs rm
+find . -type d -empty -delete
