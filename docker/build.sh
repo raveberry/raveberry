2192173512bc7e0a9fc7199b99f71d94e0060ffb
Jonathan Hacker <raveberry@jhacker.de>
2020-04-26 21:09:33 +0200

    build images for x86 and arm

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-docker build -f docker/dependencies.Dockerfile -t raveberry/raveberry-dependencies requirements/
-docker build -f docker/mopidy.Dockerfile -t raveberry/raveberry-mopidy docker/
-docker build -f docker/nginx.Dockerfile -t raveberry/raveberry-nginx docker/
+# build for the current architecture only
+#docker build -f docker/dependencies.Dockerfile -t raveberry/raveberry-dependencies requirements/
+#docker build -f docker/mopidy.Dockerfile -t raveberry/raveberry-mopidy docker/
+#docker build -f docker/nginx.Dockerfile -t raveberry/raveberry-nginx docker/
+#docker build -f docker/Dockerfile -t raveberry/raveberry .
+# build for both x64 and arm. Also pushes to dockerhub.
+docker buildx build --platform linux/amd64,linux/arm/v7 --output type=registry -f docker/dependencies.Dockerfile -t raveberry/raveberry-dependencies requirements/
+docker buildx build --platform linux/amd64,linux/arm/v7 --output type=registry -f docker/mopidy.Dockerfile -t raveberry/raveberry-mopidy docker/
+docker buildx build --platform linux/amd64,linux/arm/v7 --output type=registry -f docker/nginx.Dockerfile -t raveberry/raveberry-nginx docker/
+docker buildx build --platform linux/amd64,linux/arm/v7 --output type=registry -f docker/Dockerfile -t raveberry/raveberry .
 echo "Publish using:"
 echo "docker push raveberry/raveberry-dependencies"
 echo "docker push raveberry/raveberry-mopidy"
 echo "docker push raveberry/raveberry-nginx"
+echo "docker push raveberry/raveberry"
