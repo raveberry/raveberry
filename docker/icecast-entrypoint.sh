3a9ceb8497a96b7ac076ec6f2f16a2a40188c358
Reinier Schoof <rschoof@transip.nl>
2020-06-23 14:13:58 +0200

    made protecting stream with password optional

--- docker/icecast-entrypoint.sh
+++ docker/icecast-entrypoint.sh
@@ -2,11 +2,15 @@
 
 sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /etc/icecast.xml
 
-USER=raveberry
-PASS="$STREAM_PASSWORD"
-HASH=$(echo -n $PASS | md5sum | awk '{ print $1 }')
-ENTRY="$USER:$HASH"
-echo "$ENTRY" > /usr/share/icecast/.htaccess
-chown icecast:icecast /usr/share/icecast/.htaccess
+if [ -z "${STREAM_NOAUTH}" ]; then
+	USER=${STREAM_USERNAME:=raveberry}
+	PASS=${STREAM_PASSWORD:=raveberry}
+	HASH=$(echo -n ${PASS} | md5sum | awk '{ print $1 }')
+	echo "${USER}:${HASH}" > /usr/share/icecast/.htaccess
+	chown icecast:icecast /usr/share/icecast/.htaccess
+
+	sed -i -e 's#<!-- <authentication#<authentication#' \
+		-e 's#</authentication> -->#</authentication>#' /etc/icecast.xml
+fi
 
 exec "$@"
