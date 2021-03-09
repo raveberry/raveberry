f230871c32f9c4823eaf8df83b9c85362df89d07
Jonathan Hacker <raveberry@jhacker.de>
2021-03-09 20:39:03 +0100

    deal with type conflict

--- frontend/ts/index.d.ts
+++ frontend/ts/index.d.ts
@@ -8,12 +8,14 @@ declare let CONTROLS_ENABLED: boolean;
 declare let ADDITIONAL_KEYWORDS: string;
 declare let FORBIDDEN_KEYWORDS: string;
 
+interface JQuery {
+  // unfortunately, bootstrap and jqueryui introduce conflicts with types 'button' and 'tooltip'
+  // mock the types for the few jqueryui elements we use
+  autocomplete: any;
+  sortable: any;
+}
 interface JQueryStatic {
   keyframe: any;
+  // jqueryui
+  ui: any;
 }
-
-interface JQuery {
-  toast(...any): any;
-  modal(...any): any;
-}
-
