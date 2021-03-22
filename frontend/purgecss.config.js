21ffac015a078329f473caa68cc5c8b6deff72c8
Jonathan Hacker <raveberry@jhacker.de>
2021-03-22 10:02:43 +0100

    prevent css from being purged too zealously

--- frontend/purgecss.config.js
+++ frontend/purgecss.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   // this needs to be specified in a config file because the CLI does not handle multiple globs correctly
   content: ['ts/**/*.ts', '../templates/*.html'],
+  safelist: [
+    'collapsing', // menu transition
+    'modal-backdrop' // dimmed background behind modals
+  ],
 }
