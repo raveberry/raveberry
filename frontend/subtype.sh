64202fc93e00e5ff580ff332af33850d91f0f4e3
Jonathan Hacker <raveberry@jhacker.de>
2021-04-02 22:54:04 +0200

    remove unneeded regular font

--- frontend/subtype.sh
+++ frontend/subtype.sh
@@ -18,7 +18,7 @@ ids=$(grep -rohE 'fa-[[:alnum:]-]+' ../templates/ ts/ | sort -u | \
 	tr '\n' ',')
 
 # create subtyped fonts only containing the used glyphs
-for path in "$webfontdir"/*.woff2; do
+for path in "$webfontdir"/fa-brands-*.woff2 "$webfontdir"/fa-solid-*.woff2; do
 	file=$(basename $path)
 	echo "subsetting $file"
 	# we ignore missing glyphs as we use the same glyph set for each font file.
