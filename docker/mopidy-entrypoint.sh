265e386ecd0c1b6c9385507b1966c27c363ee719
Jonathan Hacker <raveberry@jhacker.de>
2021-09-16 16:45:42 +0200

    also read jamendo client id from env

--- docker/mopidy-entrypoint.sh
+++ docker/mopidy-entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 export MOPIDY_OUTPUT="${MOPIDY_OUTPUT:-rgvolume ! audioconvert ! audioresample ! autoaudiosink}"
 # substitute potentially set environment variables
-for KEY in MOPIDY_OUTPUT SPOTIFY_USERNAME SPOTIFY_PASSWORD SPOTIFY_CLIENT_ID SPOTIFY_CLIENT_SECRET SOUNDCLOUD_AUTH_TOKEN; do
+for KEY in MOPIDY_OUTPUT SPOTIFY_USERNAME SPOTIFY_PASSWORD SPOTIFY_CLIENT_ID SPOTIFY_CLIENT_SECRET SOUNDCLOUD_AUTH_TOKEN JAMENDO_CLIENT_ID; do
     [ -z "$(printenv ${KEY})" ] && continue
     SHORT=$(echo ${KEY#*_} | tr A-Z a-z)
     sed -i "s/^.* # ${KEY}/${SHORT} = $(printenv ${KEY})/" /config/mopidy.conf
