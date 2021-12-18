0ddde04f7c4e92b680340707c0dc9b5ee1939082
Jonathan Hacker <raveberry@jhacker.de>
2021-12-18 12:57:17 +0100

    use threads instead of celery for debug setup

--- backend/core/celery.py
+++ backend/core/celery.py
@@ -1,32 +1,55 @@
 """This module contains the celery app."""
 import os
-from celery import Celery
+from threading import Thread
+from typing import Callable, Any
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+if bool(os.environ.get("DJANGO_DEBUG")) and not bool(os.environ.get("DJANGO_CELERY")):
+    # For debugging, celery's startup is quite slow, especially when reloading on every change.
+    # Instead, use Threads to keep asynchronicity but with a much faster startup.
 
-app = Celery("core")
+    class MockCelery:
+        def task(self, function: Callable) -> Callable:
+            """This decorator mocks celery's delay function.
+            This delay() creates a thread and starts it."""
 
-app.config_from_object("django.conf:settings")
+            def delay(*args: Any, **kwargs: Any) -> None:
+                thread = Thread(target=function, args=args, kwargs=kwargs, daemon=True)
+                thread.start()
 
+            function.delay = delay
 
-class CeleryNotReachable(Exception):
-    """Raised when celery should be reachable but is not."""
+            return function
 
-    pass
+    def start() -> None:
+        pass
 
+    app = MockCelery()
+else:
+    from celery import Celery
 
-def start() -> None:
-    """Initializes celery."""
-    # check if celery is up and wait for a maximum of 5 seconds
-    for _ in range(10):
-        if app.control.ping(timeout=0.5):
-            break
-    else:
-        raise CeleryNotReachable("Celery worker pool not reachable. Is it running?")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
-    # stop running celery tasks from old django instance
-    active_tasks = app.control.inspect().active()
-    if active_tasks:
-        for hostname, tasks in active_tasks.items():
-            for task in tasks:
-                app.control.revoke(task_id=task["id"], terminate=True)
+    app = Celery("core")
+
+    app.config_from_object("django.conf:settings")
+
+    class CeleryNotReachable(Exception):
+        """Raised when celery should be reachable but is not."""
+
+        pass
+
+    def start() -> None:
+        """Initializes celery."""
+        # check if celery is up and wait for a maximum of 5 seconds
+        for _ in range(10):
+            if app.control.ping(timeout=0.5):
+                break
+        else:
+            raise CeleryNotReachable("Celery worker pool not reachable. Is it running?")
+
+        # stop running celery tasks from old django instance
+        active_tasks = app.control.inspect().active()
+        if active_tasks:
+            for hostname, tasks in active_tasks.items():
+                for task in tasks:
+                    app.control.revoke(task_id=task["id"], terminate=True)
